1ca6a1d2e4ac4f8d4f63e84f2ad4148ac38e75d9
Trayan Iliev <office@iproduct.org>
2020-01-13 18:59:05 +0200

    more error handling examples added

--- simple/errors/errors.go
+++ simple/errors/errors.go
@@ -5,6 +5,20 @@ import (
 	"time"
 )
 
+type error interface {
+	Error() string
+}
+
+type PathError struct {
+	Op string    // "open", "unlink", etc.
+	Path string  // The associated file.
+	Err error    // Returned by the system call.
+}
+
+func (e *PathError) Error() string {
+	return e.Op + " " + e.Path + ": " + e.Err.Error()
+}
+
 type MyError struct {
 	When time.Time
 	What string
@@ -26,4 +40,20 @@ func main() {
 	if err := run(); err != nil {
 		fmt.Println(err)
 	}
+
+	//var deleteTempFiles func()
+	//filename := "myfile.txt"
+	//
+	//for try := 0; try < 2; try++ {
+	//	file, err := os.Create(filename)
+	//	if err == nil {
+	//		return
+	//	}
+	//	if e, ok := err.(*os.PathError); ok && e.Err == syscall.ENOSPC {
+	//		deleteTempFiles()  // Recover some space.
+	//		continue
+	//	}
+	//	fmt.Println(file)
+	//	return
+	//}
 }
--- simple/panic/panic.go
+++ simple/panic/panic.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func badFunction() {
 	fmt.Printf("Select Panic type (0=no panic, 1=int, 2=runtime panic)\n")
@@ -28,4 +31,49 @@ func main() {
 	}()
 	badFunction()
 	fmt.Printf("Program exited normally\n")
+}
+
+// More examples:
+type Work interface{}
+
+var do func(work Work)
+
+func server(workChan <-chan *Work) {
+	for work := range workChan {
+		go safelyDo(work)
+	}
+}
+
+func safelyDo(work *Work) {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("work failed:", err)
+		}
+	}()
+	do(work)
+}
+
+// Error is the type of a parse error; it satisfies the error interface.
+type Error string
+func (e Error) Error() string {
+	return string(e)
+}
+
+// error is a method of *Regexp that reports parsing errors by
+// panicking with an Error.
+func (regexp *Regexp) error(err string) {
+	panic(Error(err))
+}
+
+// Compile returns a parsed representation of the regular expression.
+func Compile(str string) (regexp *Regexp, err error) {
+	regexp = new(Regexp)
+	// doParse will panic if there is a parse error.
+	defer func() {
+		if e := recover(); e != nil {
+			regexp = nil    // Clear return value.
+			err = e.(Error) // Will re-panic if not a parse error.
+		}
+	}()
+	return regexp.doParse(str), nil
 }
