f851ac45f5fb53cc41de77ed2a00de8198494da1
Trayan Iliev <office@iproduct.org>
2021-01-16 19:59:49 +0200

    runes demo extended with latin + chinese runes, awesomeProject added - demo using modules

--- awesomeProject/hello/hello.go
+++ awesomeProject/hello/hello.go
@@ -8,12 +8,9 @@ import (
 )
 
 func main() {
-	s := "abc日本語"
-	for index, runeValue := range s {
-		fmt.Printf("%#U starts at byte position %d\n", runeValue, index)
-	}
+	s := "Hello Go World"
 	fmt.Println(s, time.Now())
 	quote := quote.Go()
 	fmt.Println(quote)
-	fmt.Println(stringutil.Reverse(s))
+	fmt.Println(stringutil.Reverse(quote))
 }
