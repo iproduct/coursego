f49ce1c8e633e84f02cfb81074ed03de7aadbcfb
Trayan Iliev <office@iproduct.org>
2021-01-16 19:41:04 +0200

    headers added to http client request

--- lab_hello_01/hello/hello.go
+++ lab_hello_01/hello/hello.go
@@ -8,9 +8,12 @@ import (
 )
 
 func main() {
-	s := "Hello Go World"
-	fmt.Println(s + time.Now().String())
+	s := "日本語"
+	for index, runeValue := range s {
+		fmt.Printf("%#U starts at byte position %d\n", runeValue, index)
+	}
+	fmt.Println(s, time.Now())
 	quote := quote.Go()
 	fmt.Println(quote)
-	fmt.Println(stringutil.Reverse(quote))
+	fmt.Println(stringutil.Reverse(s))
 }
