2c517832b1d3c4e61484fd9d1fc5780600287d51
Trayan Iliev <office@iproduct.org>
2021-01-30 11:02:12 +0200

    module name corrected

--- 03-types-lab/type-switch/type-switch.go
+++ 03-types-lab/type-switch/type-switch.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/iproduct/03-types-lab/byteslice"
+	"github.com/iproduct/coursego/03-types-lab/byteslice"
 	"github.com/labstack/gommon/log"
 	"strconv"
 )
