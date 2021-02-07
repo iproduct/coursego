aa89906ec2094f68bff478ca065da98e139ec35d
Trayan Iliev <office@iproduct.org>
2021-02-07 18:58:54 +0200

    examples updated

--- 08-databases-gorm/entities/entities.go
+++ 08-databases-gorm/entities/entities.go
@@ -8,36 +8,35 @@ import (
 
 type Project struct {
 	gorm.Model
-	Name string 		`header:"Name"`
-	Description string 	`header:"Description"`
-	Budget float64		`header:"Budget"`
-	StartDate time.Time	`header:"Start Date"`
-	Finished bool		`header:"Completed" gorm:"'Finished' boolean"`
-	CompanyID uint      `header:"Company ID"`
-	Users []User		`gorm:"many2many:projects_users;"`
+	Name        string    `header:"Name"`
+	Description string    `header:"Description"`
+	Budget      float64   `header:"Budget"`
+	StartDate   time.Time `header:"Start Date"`
+	Finished    bool      `header:"Completed" gorm:"'Finished' boolean"`
+	CompanyID   uint      `header:"Company ID"`
+	Users       []User    `gorm:"many2many:projects_users;"`
 }
 
 type Company struct {
 	gorm.Model
-	Name string 		`header:"Name"`
+	Name     string `header:"Name"`
 	Projects []Project
 }
 
 type User struct {
 	gorm.Model
-	FirstName string 	`header:"First Name"`
-	LastName string 	`header:"Last Name"`
-	Email string 		`header:"Email"`
-	Username string 	`header:"Username"`
-	Password string 	`header:"Password"`
-	Active bool `header:"Active" xorm:"'Active' boolean"`
-	Projects []Project		`gorm:"many2many:projects_users;"`
+	FirstName string    `header:"First Name"`
+	LastName  string    `header:"Last Name"`
+	Email     string    `header:"Email"`
+	Username  string    `header:"Username"`
+	Password  string    `header:"Password"`
+	Active    bool      `header:"Active" gorm:"'Active' boolean"`
+	Projects  []Project `gorm:"many2many:projects_users;"`
 }
 
 func (user *User) BeforeSave(tx *gorm.DB) (err error) {
 	if pw, err := bcrypt.GenerateFromPassword([]byte(user.Password), 5); err == nil {
-		tx.Statement.SetColumn("Password", "{bcrypt}" + string(pw))
+		tx.Statement.SetColumn("Password", "{bcrypt}"+string(pw))
 	}
 	return
 }
-
