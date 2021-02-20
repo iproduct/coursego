432e663ae5bcb05d5540df969db25ddbba8c04a4
Trayan Iliev <office@iproduct.org>
2021-02-20 15:41:24 +0200

    graphql simple demo added

---
+++ 11-graphql-simple-lab/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	gqhandler "github.com/graphql-go/graphql-go-handler"
+	"github.com/iproduct/coursego/11-graphql-simple-lab/schema"
+	"log"
+	"net/http"
+)
+
+func main() {
+	handler := gqhandler.New(&gqhandler.Config{
+		Schema: &schema.Schema,
+		Pretty: true,
+	})
+
+	http.Handle("/graphql", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
