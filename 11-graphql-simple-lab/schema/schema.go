bac1d1f10f965432394b32dce0ad383a53c17fea
Trayan Iliev <office@iproduct.org>
2021-02-20 15:30:37 +0200

    graphql lab added

---
+++ 11-graphql-simple-lab/schema/schema.go
@@ -0,0 +1,19 @@
+package schema
+
+import (
+	"github.com/graphql-go/graphql"
+)
+
+var queryType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Query",
+	Fields: graphql.Fields{
+		"latestPost": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return "Hello GraphQL!", nil
+			},
+		},
+	},
+})
+
+var Schema, _ = graphql.NewSchema(graphql.SchemaConfig{Query: queryType})
