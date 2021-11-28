903096633b4572f9522bd648c29e0532f67a029e
Trayan Iliev <office@iproduct.org>
2021-11-28 22:31:16 +0200

    json.Encoder and json.Decoder demos added

--- 03-types-lab2/json/main.go
+++ 03-types-lab2/json/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/iproduct/coursego/03-types-lab2/json/books"
 	"log"
+	"strings"
 )
 
 func main() {
@@ -27,7 +28,27 @@ func main() {
 	if err := json.Unmarshal(data, &books); err != nil {
 		log.Fatalf("JSON unmarshaling failed: %s", err)
 	}
-	fmt.Println("AFTER UNMARSHAL:\n")
+	fmt.Println("\nAFTER UNMARSHAL:")
+	for i, book := range books {
+		fmt.Printf("%d: %#v\n", i, book)
+	}
+
+	// Using json.Encoder
+	fmt.Println("\nUSING ENCODER:")
+	writer := &strings.Builder{}
+	encoder := json.NewEncoder(writer)
+	err = encoder.Encode(books)
+	if err != nil {
+		log.Fatalf("JSON marshaling failed: %s", err)
+	}
+	fmt.Printf("%s\n", writer.String())
+
+	//// JSON -> structs
+	decoder := json.NewDecoder(strings.NewReader(writer.String()))
+	if err := decoder.Decode(&books); err != nil {
+		log.Fatalf("JSON unmarshaling failed: %s", err)
+	}
+	fmt.Println("AFTER DECODER UNMARSHAL:")
 	for i, book := range books {
 		fmt.Printf("%d: %#v\n", i, book)
 	}
