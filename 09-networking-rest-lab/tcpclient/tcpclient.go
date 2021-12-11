b3fdb83dfde5098b5b93a1690d2d8269861ec766
Trayan Iliev <office@iproduct.org>
2021-12-11 17:33:22 +0200

    tcp client added

---
+++ 09-networking-rest-lab/tcpclient/tcpclient.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	var d net.Dialer
+	ctx, _ := context.WithTimeout(context.Background(), time.Second)
+	conn, err := d.DialContext(ctx, "tcp", "127.0.0.1:8080")
+	defer conn.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if _, err := conn.Write([]byte("Hello World\n")); err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(conn)
+	i := 0
+	for i < 2 && scanner.Scan() {
+		message := scanner.Text()
+		fmt.Println("Message received: ", message)
+	}
+	if err := scanner.Err(); err != nil {
+		log.Printf("Error scanning server response: %v\n", err)
+	}
+	log.Printf("Closing client connection %v\n", conn)
+}
