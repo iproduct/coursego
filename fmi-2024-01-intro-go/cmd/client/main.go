8e100b55302593577432b4b97074d2b4c37904bb
Trayan Iliev <office@iproduct.org>
2024-10-07 21:35:27 +0300

    http client cmd added

---
+++ fmi-2024-01-intro-go/cmd/client/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	//resp, err := http.Get("http://googleapis.com/books/v1/volumes?q=golang")
+	req, err := http.NewRequest("GET", "https://www.googleapis.com/books/v1/volumes?q=golang", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Add("Accept", "application/json")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Response Status: %v\n", resp.Status)
+	scanner := bufio.NewScanner(resp.Body)
+	for i := 0; scanner.Scan() && i < 20; i++ {
+		fmt.Println(scanner.Text())
+	}
+
+}
