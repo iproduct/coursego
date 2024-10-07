c62f6800bafa2c4337ce814d2baf796f67b10532
Trayan Iliev <office@iproduct.org>
2024-10-07 20:52:04 +0300

    reverse func added

--- fmi-2024-01-intro-go/cmd/hello/main.go
+++ fmi-2024-01-intro-go/cmd/hello/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/iproduct/coursego/fmi-2024-01-intro-go/stringutil"
 )
 
 func main() {
 	s := "Hello Go World"
-	fmt.Println(s)
+	result := stringutil.Reverse(s)
+	fmt.Println(result)
 }
--- fmi-2024-01-intro-go/go.mod
+++ fmi-2024-01-intro-go/go.mod
@@ -1,3 +1,5 @@
 module github.com/iproduct/coursego/fmi-2024-01-intro-go
 
 go 1.23.2
+
+require github.com/iproduct/coursego/fmi-2023-01-intro-lab v0.0.0-20241007172140-a4b4abab221a
