9d82530af124ba3d615b8c5e1807011cef4ff468
Trayan Iliev <office@iproduct.org>
2024-10-28 19:26:19 +0200

    wordcount demo problem + unit test complete

--- fmi-2024-01-intro-go/wordcount/wordcount.go
+++ fmi-2024-01-intro-go/wordcount/wordcount.go
@@ -1,5 +1,12 @@
 package wordcount
 
+import "strings"
+
 func WordCount(s string) map[string]int {
-	return map[string]int{"x": 0, "y": 0, "z": 0}
+	counts := make(map[string]int)
+	words := strings.Fields(s)
+	for _, word := range words {
+		counts[word]++
+	}
+	return counts
 }
